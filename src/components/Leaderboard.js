7cd646e3d12be306d79c6c0517ebc49e2d8a819b
leynad <matei_daniel@outlook.com>
2023-04-03 18:14:31 +0200

    Add feature to display leaderboard in ascendent order

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../config/firebase";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, orderBy,query } from "firebase/firestore";
 import "../styles/Leaderboard.css"
 function Leaderboard() {
   const [leaderboardData, setLeaderboardData] = useState([]);
-
+  const [position, setPosition] = useState(1)
   async function getLeaderboardData() {
     
     try {
-      const querySnapshot = await getDocs(collection(db, "leaderboard"));
+      const querySnapshot = await getDocs(query(collection(db, "leaderboard")),orderBy('time_seconds','asc'));
       const data = [];
       querySnapshot.forEach((doc) => {
         const timestamp = doc.data().createdAt;
@@ -16,7 +16,11 @@ function Leaderboard() {
         const dateString = date.toLocaleString();
         data.push({ ...doc.data(), timestamp: dateString });
       });
+
+      data.sort((a,b)=>a.time_seconds - b.time_seconds)
+      console.log(data)
       setLeaderboardData(data);
+
     } catch (error) {
       console.error("Error fetching leaderboard data: ", error);
     }
@@ -31,9 +35,12 @@ function Leaderboard() {
 
   return (
     <div className="leaderboard-wrapper">
+
+      <div className="winners-title">Check the winners leaderboard here!</div>
       <table>
       <thead>
         <tr>
+          <th>Position</th>
           <th>Name</th>
           <th>Time</th>
           <th>Date</th>
@@ -41,8 +48,10 @@ function Leaderboard() {
         </thead>
         <tbody>
         
-        {leaderboardData.map((user)=>(
-          <tr key={user.id}>
+        {leaderboardData.map((user,index)=>(
+
+          <tr key={index}>
+          <td className="user">{index+1}</td>  
           <td className="user">{user.name}</td>
           <td className="user">{user.time_minutes}</td>
           <td className="user">{user.timestamp}</td>
