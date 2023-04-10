e76eb6ca62f9661fe8379b719a0d0aa42b7fe5e0
leynad <matei_daniel@outlook.com>
2023-04-10 23:48:34 +0200

    Add router change - mimic successful experiment with another project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,23 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import Timer from "./Timer";
 
-function Navbar({ gameLoaded, seconds, setSeconds,start, setStart }) {
-
+function Navbar({ gameLoaded, seconds, setSeconds, start, setStart }) {
   return (
     <div>
       <header>
-        <nav >
+        <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/leaderboard">Leaderboard</Link>
         </nav>
         <div>
           {gameLoaded && (
-            <Timer start={start} setStart={setStart} seconds={seconds} setSeconds={setSeconds} />
+            <Timer
+              start={start}
+              setStart={setStart}
+              seconds={seconds}
+              setSeconds={setSeconds}
+            />
           )}
         </div>
       </header>
